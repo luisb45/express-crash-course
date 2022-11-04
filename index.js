952b4b1233aa5a4bb5c6f372c3922740053c32f7
Luis Bustos <luisbustos500@gmail.com>
2022-11-03 18:17:47 -0600

    homepage route

--- index.js
+++ index.js
@@ -1,10 +1,30 @@
 const express = require('express');
+const path = require('path');
+const exphbs = require('express-handlebars');
+const logger = require('./middleware/logger');
 
 const app = express();
 
-app.get('/', (req, res) => {
-    res.send('<h1>Hello World</h1>');
-});
+
+//Init middleware
+//app.use(logger);
+
+//HandleBars Middleware
+app.engine("handlebars", exphbs.engine({ defaultLayout: "main" }));
+app.set('view engine', 'handlebars');
+
+//Body Parser Middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: false}));
+
+//Homepage route
+app.get('/', (req, res) => res.render('index'));
+
+//set static folder
+app.use(express.static(path.join(__dirname, 'public')));
+
+//Members API Routes
+app.use('/api/members', require('./routes/api/members'));
 
 const PORT = process.env.PORT || 5000;
 
